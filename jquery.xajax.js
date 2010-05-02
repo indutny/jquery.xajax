4ed6cbc4c30e8f1b0b888e042863cf1692e339e6
Fedor Indutny <fedor.indutny@gmail.com>
2010-05-02 17:27:43 +0700

    CRLF fix

--- jquery.xajax.js
+++ jquery.xajax.js
@@ -1,1 +1,55 @@
-(function ($, undefined)  {	function tempObj() {		return $("<b/>");	}		function wrapper( callback, obj) {		return function () {			callback && obj.before(callback.apply(this, arguments)).remove();		}	}		$.xajax = function ( conf ) {			var			obj = tempObj();				conf.success = wrapper(conf.success, obj);		obj.xhr = $.ajax(conf);				return obj;	}		$.xget = function ( url, data, callback, type) {			var 			obj  = tempObj();					if ( $.isFunction( data ) ) {			type = type || callback;			callback = data;			data = null;		}				obj.xhr = $.get( url, data, wrapper( callback,  obj));				return obj;	}		$.xpost = function ( url, data, callback, type) {			var 			obj  = tempObj();					if ( $.isFunction( data ) ) {			type = type || callback;			callback = data;			data = {};		}				obj.xhr = $.post( url, data, wrapper( callback,  obj));				return obj;	}	})(jQuery);/**@preserve jQuery.xAjax plugin v.0.0.1;Copyright 2010, Fedor Indutny;Released under MIT license **/
+(function ($, undefined)  {
+	function tempObj() {
+		return $("<b/>");
+	}
+	
+	function wrapper( callback, obj) {
+		return function () {
+			callback && obj.before(callback.apply(this, arguments)).remove();
+		}
+	}
+	
+	$.xajax = function ( conf ) {
+	
+		var
+			obj = tempObj();
+		
+		conf.success = wrapper(conf.success, obj);
+		obj.xhr = $.ajax(conf);
+		
+		return obj;
+	}
+	
+	$.xget = function ( url, data, callback, type) {
+	
+		var 
+			obj  = tempObj();
+			
+		if ( $.isFunction( data ) ) {
+			type = type || callback;
+			callback = data;
+			data = null;
+		}
+		
+		obj.xhr = $.get( url, data, wrapper( callback,  obj));
+		
+		return obj;
+	}
+	
+	$.xpost = function ( url, data, callback, type) {
+	
+		var 
+			obj  = tempObj();
+			
+		if ( $.isFunction( data ) ) {
+			type = type || callback;
+			callback = data;
+			data = {};
+		}
+		
+		obj.xhr = $.post( url, data, wrapper( callback,  obj));
+		
+		return obj;
+	}
+	
+})(jQuery);/**@preserve jQuery.xAjax plugin v.0.0.1;Copyright 2010, Fedor Indutny;Released under MIT license **/
